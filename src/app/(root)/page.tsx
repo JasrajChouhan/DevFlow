8cb00ff4510f9b504cef703ea8ba1d239411a521
JasrajChouhan <jasrajc945@gmail.com>
2025-01-16 23:05:25 +0530

    feat: add Tailwind CSS typography plugin, new SVG icons, and update Home component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <p className=" bg-red-400 text-3xl text-pink-400">Hello world</p>;
-      <p className=" bg-red-400 text-3xl font-bold text-pink-400">
-        Hello world
-      </p>
-      ;
+      <p>Hello world</p>
     </div>
   );
 }
