26339530b914dc43a2e8ad5bc6d10b2e359d6a44
JasrajChouhan <jasrajc945@gmail.com>
2025-01-16 23:11:16 +0530

    feat: enhance RootLayout with Navbar and improved structure

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,21 @@
+import Navbar from "@/components/navigation/navbar";
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <main className="background-light850_dark100">
+      <Navbar />
+
+      <div className="flex">
+        {/* left sidebar */}
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+
+        {/* left sidebar */}
+      </div>
+    </main>
+  );
 };
 
 export default RootLayout;
