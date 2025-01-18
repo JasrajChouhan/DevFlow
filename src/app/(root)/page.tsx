7b0e8d303e4cfb886a6b3d924b99837968a974c8
JasrajChouhan <jasrajc945@gmail.com>
2025-01-19 03:28:13 +0530

    feat: enhance Home component with 'Ask a Question' button and add ASK_QUESTION route

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { ROUTES } from "@/constants/routes";
+
 export default function Home() {
   return (
-    <div>
-      <p>Hello world</p>
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+    </>
   );
 }
