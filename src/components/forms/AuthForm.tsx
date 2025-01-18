be2e2fd275eb5dcf32b0b02f541e1eed5316741d
JasrajChouhan <jasrajc945@gmail.com>
2025-01-19 03:17:18 +0530

    fix: update useRouter import to use next/navigation in AuthForm component

--- src/components/forms/AuthForm.tsx
+++ src/components/forms/AuthForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import {
   DefaultValues,
   FieldValues,
