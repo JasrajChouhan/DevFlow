ca41037a106fd9d5604f3b259399fd84fad2838b
JasrajChouhan <jasrajc945@gmail.com>
2025-01-20 03:33:41 +0530

    feat: add TypeScript interfaces for Tag, Author, and Question types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -7,3 +7,25 @@ type ActionResponse<T = null> = {
   };
   status?: number;
 };
+
+interface Tag {
+  _id: string;
+  name: string;
+}
+
+interface Author {
+  _id: string;
+  name: string;
+  image: string;
+}
+interface Question {
+  _id: string;
+  title: string;
+  tags: Tag[];
+  author: Author;
+  upvotes: number;
+  answers: number;
+  views: number;
+  createdAt: Date;
+}
+
