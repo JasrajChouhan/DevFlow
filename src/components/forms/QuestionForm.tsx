484429de54e88332421937e3ada46a6b4e120d77
JasrajChouhan <jasrajc945@gmail.com>
2025-01-20 04:46:41 +0530

    feat: integrate MDX editor into QuestionForm for enhanced question submission

--- src/components/forms/QuestionForm.tsx
+++ src/components/forms/QuestionForm.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { MDXEditorMethods } from "@mdxeditor/editor";
+import dynamic from "next/dynamic";
+import { useRef } from "react";
 import { useForm } from "react-hook-form";
 
 import { Button } from "../ui/button";
@@ -17,7 +20,12 @@ import {
 import { Input } from "@/components/ui/input";
 import { AskQuestionSchema } from "@/schema/form-shema";
 
+const Editor = dynamic(() => import("../editor/Editor"), {
+  ssr: false,
+});
+
 const QuestionForm = () => {
+  const editorRef = useRef<MDXEditorMethods>(null);
   const form = useForm({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
@@ -68,7 +76,13 @@ const QuestionForm = () => {
                 Detailed explanation of your problem{" "}
                 <span className="text-primary-500">*</span>
               </FormLabel>
-              <FormControl>Editor</FormControl>
+              <FormControl>
+                <Editor
+                  editorRef={editorRef}
+                  fieldChange={field.onChange}
+                  value={field.value}
+                />
+              </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Introduce the problem and expand on what you&apos;ve put in the
                 title.
