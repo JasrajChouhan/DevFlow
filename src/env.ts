31a67689803526d20c22b7b7ba519c1332424c89
JasrajChouhan <jasrajc945@gmail.com>
2025-01-19 13:20:49 +0530

    feat: add environment configuration and utility for retrieving environment variables

---
+++ src/env.ts
@@ -0,0 +1,24 @@
+import { logLevels } from "./lib/logger";
+
+export interface Enivorment {
+  NODE_ENV: "development" | "production";
+  LOG_LEVEL: keyof typeof logLevels;
+}
+
+// get env
+
+export const getEnv = <K extends keyof Enivorment>(
+  k: K,
+  fallback?: Enivorment[K]
+): Enivorment[K] => {
+  const value = process.env[k] as Enivorment[K] | undefined;
+
+  if (!value) {
+    if (fallback) {
+      return fallback;
+    } else {
+      throw new Error(`Missing env variable: ${k}`);
+    }
+  }
+  return value;
+};
