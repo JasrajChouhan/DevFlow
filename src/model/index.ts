ed834e41b6752c41a836d261e3439099695421ea
JasrajChouhan <jasrajc945@gmail.com>
2025-01-19 10:28:20 +0530

    feat: add index file to export mongoose models for easier access

---
+++ src/model/index.ts
@@ -0,0 +1,9 @@
+import Account from "./account.model";
+import Answer from "./answer.model";
+import Collection from "./collection.model";
+import Question from "./question.model";
+import TagQuestion from "./tag-question.model";
+import Tag from "./tag.model";
+import User from "./user.model";
+
+export { Account, Answer, Collection, Question, Tag, TagQuestion, User };
