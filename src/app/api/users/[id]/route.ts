f741e2b795f3d2b42d730ab473cc02603e1016f9
JasrajChouhan <jasrajc945@gmail.com>
2025-01-21 05:15:51 +0530

    feat: implement user update API with validation and error handling

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 
 import { handleError } from "@/lib/hanlder/error";
-import { NotFoundError } from "@/lib/http-errors";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
 import connectToDB from "@/lib/mongoose";
 import { User } from "@/model";
+import { userSchema } from "@/schema/form-shema";
 import { APIErrorResponse } from "@/types/global";
 
 // Get a user which a userID {id}
@@ -47,6 +48,42 @@ export async function DELETE(
   try {
     await connectToDB();
     const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      throw new NotFoundError("User not found");
+    }
+    return NextResponse.json(
+      {
+        success: true,
+        data: user,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+// update a user
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) {
+    throw new NotFoundError("User not found");
+  }
+  try {
+    await connectToDB();
+    const body = await request.json();
+    const validatedData = userSchema.partial().safeParse(body);
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+    const user = await User.findByIdAndUpdate(id, validatedData.data, {
+      new: true,
+    });
     if (!user) {
       throw new NotFoundError("User not found");
     }
