2e0debe87ecfe27170b05dffa0cefcd338cfb639
JasrajChouhan <jasrajc945@gmail.com>
2025-01-21 01:35:32 +0530

    feat: add interaction model with schema and interfaces for user actions

---
+++ src/model/innteraction.model.ts
@@ -0,0 +1,38 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IInteraction {
+  user: Types.ObjectId;
+  action: string;
+  actionId: Types.ObjectId;
+  actionType: "question" | "answer";
+}
+export interface IInteractionDoc extends IInteraction, Document {}
+export const InteractionSchema = new Schema<IInteraction>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    action: {
+      type: String,
+      required: true,
+    },
+    actionId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    actionType: {
+      type: String,
+      enum: ["question", "answer"],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Interaction =
+  models?.Interaction || model<IInteraction>("Interaction", InteractionSchema);
