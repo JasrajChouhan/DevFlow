8a0eb81a6206c7767941f3d13398a842b7b45ef8
JasrajChouhan <jasrajc945@gmail.com>
2025-01-21 05:03:18 +0530

    feat: implement user creation API with validation and error handling

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,8 +3,10 @@
 import { NextResponse } from "next/server";
 
 import { handleError } from "@/lib/hanlder/error";
+import { ValidationError } from "@/lib/http-errors";
 import connectToDB from "@/lib/mongoose";
 import { User } from "@/model";
+import { userSchema } from "@/schema/form-shema";
 import { APIErrorResponse } from "@/types/global";
 
 export async function GET() {
@@ -18,6 +20,46 @@ export async function GET() {
       },
       {
         status: 200,
+      }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await connectToDB();
+    const body = await request.json();
+
+    const validatedData = userSchema.safeParse(body);
+
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    const { email, username } = validatedData.data;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new Error("User already exists");
+    }
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      throw new Error("Username already exists");
+    }
+
+    // create new user
+    const newUser = await User.create(validatedData.data);
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: newUser,
+      },
+      {
+        status: 201,
       }
     );
   } catch (error) {
