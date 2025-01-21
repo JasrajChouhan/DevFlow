acd43fb4bae628f936b8c3145ab4f0c7dad9c2e3
JasrajChouhan <jasrajc945@gmail.com>
2025-01-21 05:53:31 +0530

    feat: implement POST API for creating user accounts with validation and error handling

--- src/app/api/users/accounts/route.ts
+++ src/app/api/users/accounts/route.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
 
 import { handleError } from "@/lib/hanlder/error";
+import { ForbiddenError, ValidationError } from "@/lib/http-errors";
 import connectToDB from "@/lib/mongoose";
 import { Account } from "@/model";
+import { accountSchema } from "@/schema/form-shema";
 import { APIErrorResponse } from "@/types/global";
 
 export async function GET() {
@@ -16,6 +18,46 @@ export async function GET() {
       },
       {
         status: 200,
+      }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+// create account
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const validatedData = accountSchema.safeParse(body);
+
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    const { providerAccountId, provider } = validatedData.data;
+
+    const existingAccount = await Account.findOne({
+      provider,
+      providerAccountId,
+    });
+
+    if (existingAccount) {
+      throw new ForbiddenError(
+        "An account with the same provider already exists"
+      );
+    }
+
+    // crate new account
+    const newAccount = await Account.create(validatedData);
+    return NextResponse.json(
+      {
+        success: true,
+        data: newAccount,
+      },
+      {
+        status: 201,
       }
     );
   } catch (error) {
