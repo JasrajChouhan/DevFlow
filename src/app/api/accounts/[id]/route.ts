15c35880c6548b4920bd8fbc0a57bc8cf10242b1
JasrajChouhan <jasrajc945@gmail.com>
2025-01-21 08:29:39 +0530

    feat: implement account update api end point

--- src/app/api/accounts/[id]/route.ts
+++ src/app/api/accounts/[id]/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 
 import { handleError } from "@/lib/hanlder/error";
-import { NotFoundError } from "@/lib/http-errors";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
 import connectToDB from "@/lib/mongoose";
 import { Account } from "@/model";
+import { accountSchema } from "@/schema/form-shema";
 import { APIErrorResponse } from "@/types/global";
 
 export async function GET(
@@ -52,6 +53,41 @@ export async function DELETE(
       {
         success: true,
         data: Account,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) {
+    throw new NotFoundError("Account not found");
+  }
+  try {
+    await connectToDB();
+    const body = await request.json();
+    const validatedData = accountSchema.partial().safeParse(body);
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+    const account = await Account.findByIdAndUpdate(id, validatedData.data, {
+      new: true,
+    });
+    if (!account) {
+      throw new NotFoundError("Account not found");
+    }
+    return NextResponse.json(
+      {
+        success: true,
+        data: account,
       },
       {
         status: 200,
