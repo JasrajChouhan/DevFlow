72ba89f3e406d6fd6941e6add55a3f05354a841a
JasrajChouhan <jasrajc945@gmail.com>
2025-01-21 08:25:32 +0530

    feat: get dynamic account by id.

---
+++ src/app/api/accounts/[id]/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+
+import { handleError } from "@/lib/hanlder/error";
+import { NotFoundError } from "@/lib/http-errors";
+import connectToDB from "@/lib/mongoose";
+import { Account } from "@/model";
+import { APIErrorResponse } from "@/types/global";
+
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    await connectToDB();
+    const { id } = await params;
+    if (!id) {
+      throw new NotFoundError("Account not found.");
+    }
+    const account = await Account.findById(id);
+    if (!account) {
+      throw new NotFoundError("Account not found");
+    }
+    return NextResponse.json(
+      {
+        success: true,
+        data: account,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
